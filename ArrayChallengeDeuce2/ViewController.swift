a09c6c37f5bea037d8422125f1d87259903cab82
molotov2k <molotov2k@gmail.com>
2016-07-07 14:22:22 -0400

    done

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -18,10 +18,61 @@ class ViewController: UIViewController {
     }
 
    
-    
-    
     // Create your methods here
-
-
+    
+    func addNameToDeliLine(name:String) -> String {
+        
+        let greeting: String
+        
+        switch name {
+        case "Billy Crystal":
+            deliLine.insert(name, atIndex: 0)
+            greeting = "Welcome Billy! You can sit wherever you like."
+        case "Meg Ryan":
+            deliLine.insert(name, atIndex: 0)
+            greeting = "Welcome Meg! You can sit wherever you like."
+        default:
+            deliLine.append(name)
+            if deliLine.count == 1 {
+                greeting = "Welcome \(name), you're first in line!"
+            } else {
+                greeting = "Welcome \(name), you're number \(deliLine.count) in line."
+            }
+        }
+        
+        return greeting
+    }
+    
+    
+    func nowServing() -> String {
+        
+        let result: String
+        
+        if deliLine.isEmpty {
+            result = "There is no-one to be served."
+        } else {
+            let name = deliLine.removeAtIndex(0)
+            result = "Now serving \(name)"
+        }
+        
+        return result
+    }
+    
+    
+    func deliLineDescription() -> String {
+        
+        var line = ""
+        
+        if deliLine.isEmpty {
+            line = "The line is currently empty."
+        } else {
+            for (index, name) in deliLine.enumerate() {
+                line += "\(index + 1). \(name)\n"
+            }
+        }
+        
+        return line
+    }
+    
 }
 
